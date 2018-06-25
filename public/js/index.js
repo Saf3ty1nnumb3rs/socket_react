31d53166329be672344ae13ccd6a634e74799a5e
Josh Sample <joshuawsample@gmail.com>
2018-06-25 00:28:48 -0400

    Added jQuery and added form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,29 @@
 const socket = io()
-
+    // Client Connect
     socket.on('connect', function() {
         console.log('Connected to server.')
 
     })
+    // Client Disconnect
     socket.on('disconnect', function() {
         console.log('Disconnected from server')
     })
 
-
+    //New message call
     socket.on('newMessage', function(message) {
         console.log('New message', message)
+        let li = jQuery('<li></li>')
+        li.text(`${message.from}: ${message.text}`)
+
+        jQuery('#messages').append(li)
+    })
+
+    jQuery('#message-form').on('submit', function (e) {
+        e.preventDefault()
+        socket.emit('createMessage', {
+            from: 'User',
+            text: jQuery('[name=message]').val()
+        }, function(){
+
+        })
     })
