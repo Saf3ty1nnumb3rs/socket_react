b2661714ce0285a1a71442be7d5e948467ba0c2c
Josh Sample <joshuawsample@gmail.com>
2018-06-28 11:40:40 -0400

    Test for generateLocationMessage passed

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         const from = 'Jen'
@@ -10,5 +10,19 @@ describe('generateMessage', () => {
         expect(message.createdAt).toBeA('number')
         expect(message).toInclude({from, text})
         
+    })
+})
+
+describe('generateLocationMesseage', () => {
+    it('Should generate correct location object', () => {
+        const from = 'Your mom';
+        const latitude = 69;
+        const longitude = 86;
+        const url = 'https://www.google.com/maps?q=69,86';
+        const message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number')
+        expect(message).toInclude({from, url})
+
     })
 })
