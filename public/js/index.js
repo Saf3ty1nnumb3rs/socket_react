3524c254443a01efae9653e63debe757b47fc293
Josh Sample <joshuawsample@gmail.com>
2018-07-03 21:23:15 -0400

    Added dynamic chat room availability

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,25 @@
-document.getElementById('room-name').addEventListener('focus', () => {
-    var httpRequest = new XMLHttpRequest()
-    httpRequest.open('GET', '/rooms')
-    
-    httpRequest.onreadystatechange =() => {
-       console.log(httpRequest)
-}
-    httpRequest.send()
-})
+document.getElementById("room-name").addEventListener("focus", () => {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/rooms");
+  xhr.setRequestHeader("Content-Type", "text/xml");
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = xhr.responseText;
+        console.log(data);
+        var jsonResponse = JSON.parse(data);
+        let list = document.getElementById("room-list");
+        jsonResponse.forEach(room => {
+          console.log(room);
+          let option = document.createElement("option");
+          option.value = room;
+          list.appendChild(option);
+        });
+        console.log(list)
+        // document.getElementById('room-list').appendChild(list)
+      }
+    }
+  };
+  xhr.send();
+  
+});
